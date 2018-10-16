8d37acfad5ed1059a1b1809e907e2454abf4ae69
Jake Wheeler <jakew@cse.unr.edu>
2018-10-16 12:54:22 -0700

    Removed unused code

--- inventory/views.py
+++ inventory/views.py
@@ -1,18 +1,3 @@
-#from django.http import JsonResponse
-#from django.shortcuts import render
-#from django.http import HttpResponse
-#from django.template import Template
-#import json
-#from django.core import serializers
-
-#def EquipmentList(request):
-#    Equipment_As_Table = Equipment.objects.all()
-#    return render(request, 'lists.html')
-#def JsonView(request):
- #   Equipment_as_json = serializers.serialize('json',Equipment.objects.all(), fields = ('id',))
-  #  return JsonResponse(Equipment_as_json,safe=False)
-
-
 from django.shortcuts import render
 from rest_framework import viewsets, response
 from .models import Equipment
