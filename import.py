55b1525f97c7618400f4979e1b85ced888defc0a
Jake Wheeler <jakewhlr@gmail.com>
2018-10-26 02:29:40 -0700

    updated import script

--- import.py
+++ import.py
@@ -1,53 +1,37 @@
 #!/usr/bin/env python3
 
+import os, django
 import argparse
 import csv
 import datetime
 
-parser = argparse.ArgumentParser(description='Imports Nova Geotechnical inventory CSV to Django database.')
-parser.add_argument('--file', metavar='-f', type=argparse.FileType('r'), help="Input file in CSV format.")
-args = parser.parse_args()
-
-equipment = [] # empty equipment list to append to
-
-reader = csv.reader(args.file, delimiter=',', quotechar='|')
-next(reader)
-for row in reader:
-    equipment.append({'Equipment_Type': row[0].split('-')[0],
-                      'Inventory_Number': row[0].split('-')[1],
-                      'Description_of_Item': row[1],
-                      'Manufacturer': row[2],
-                      'Model_Number': row[3],
-                      'Serial_Number': row[4],
-                      'Condition_as_recieved': row[5],
-                      # 'Date_revievd': row[6],
-                      #'Calibration_Date': row[8][:-2] + '20' + row[8][-2:],
-                      'Calibration_Date' : datetime.datetime.strptime(row[6],'%m/%d/%Y'),
-                      'Calibration_Frequency': row[7],
-    })
-    print(row[6])
-
-
-import os, django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nova_geotechnical.settings")
 django.setup()
 
 from inventory.models import *
 
-for item in equipment:
-    added_item = Equipment()
-    added_item.equipment_type = item['Equipment_Type'] # list convert
-    added_item.inventory_number = item['Inventory_Number']
-    added_item.description = item['Description_of_Item']
-    added_item.manufacturer = item['Manufacturer']
-    added_item.model_number = item['Model_Number']
-    added_item.serial_number = item['Serial_Number']
-    added_item.condition_as_recieved = item['Condition_as_recieved'] # list convert
-    #added_item.Calibration_Date = datetime.datetime.strptime(item['Calibration_Date'], '%m/%d/%y') # to datetime?
-    added_item.calibration_date = item['Calibration_Date']
-    added_item.calibration_frequency = int(item['Calibration_Frequency'])
-    # added_item.Due_Date = item['Due_Date'] # to datetime?
-    print(type(added_item.calibration_date))
-    added_item.save()
+parser = argparse.ArgumentParser(description='Imports Nova Geotechnical inventory CSV to Django database.')
+parser.add_argument('-f', '--file', type=argparse.FileType('r'), help="Input file in CSV format.")
+args = parser.parse_args()
 
-print(Equipment.objects.all())
+reader = csv.reader(args.file, delimiter=',', quotechar='|')
+next(reader)
+for row in reader:
+    print("Importing item " + row[0])
+    added_item = Equipment()
+    added_item.equipment_type = row[0].split('-')[0]
+    added_item.inventory_number = row[0].split('-')[1]
+    added_item.description = row[1]
+    added_item.manufacturer = row[2]
+    added_item.model_number = row[3]
+    added_item.serial_number = row[4]
+    added_item.condition_as_recieved = row[5]
+    if row[6]:
+        added_item.calibration_date = datetime.datetime.strptime(row[6],'%m/%d/%Y')
+    added_item.calibration_frequency = int(row[7])
+    try:
+        added_item.save()
+        print('\033[92m' + "Success!" + '\033[0m')
+    except Exception as err:
+        print('\033[91m' + str(err) + '\033[0m')
+    print('')
--- inventory/models.py
+++ inventory/models.py
@@ -4,15 +4,15 @@ from dateutil.relativedelta import relativedelta
 
 class Equipment (models.Model):
     EQUIPMENT_TYPES = (
-        ('Tool','Tool'),
+        ('TL','Tool'),
         ('BA','BA'),
-        ('Sieve','Sieve'),
-        ('Compaction Mold','Compaction Mold'),
-        ('Cleanness Sieve','Cleanness Sieve'),
-        ('Soundness Sieve','Soundness Sieve'),
-        ('Marshall Aaparatus','Marshall Aparatus'),
-        ('Slump Set','Slump Set'),
-        ('Slump set thermometer','Slump Set Thermometer')
+        ('SV','Sieve'),
+        ('CM','Compaction Mold'),
+        ('CS','Cleanness Sieve'),
+        ('SC','Soundness Sieve'),
+        ('MA','Marshall Aparatus'),
+        ('SS','Slump Set'),
+        ('SST','Slump Set Thermometer')
     )
     CONDITIONS = (
         ('New','New'),
@@ -41,8 +41,8 @@ class Equipment (models.Model):
     model_number = models.CharField(blank=True, null=True, max_length=200,default=None)
     serial_number = models.CharField(blank=True, null=True, max_length=200,default=None)
     condition_as_recieved = models.CharField(blank=True, null=True, max_length=10, default='New', choices=CONDITIONS)
-    calibration_date = models.DateField()
-    due_date = models.DateField()
+    calibration_date = models.DateField(null=True)
+    due_date = models.DateField(null=True)
     calibration_frequency = models.IntegerField(blank=True, null=True, help_text='months')
     calibrated_by = models.CharField(blank=True, null=True, max_length=200,default=None, choices=CALIBRATORS)
     status = models.CharField(blank=True, null=True, max_length=200,default=None, choices=STATUSES)
@@ -56,7 +56,8 @@ class Equipment (models.Model):
 
     def save(self, *args, **kwargs):
         self.inventory_tag = self.equipment_type + '-' + str(self.inventory_number)
-        self.due_date = self.calibration_date + relativedelta(months=+self.calibration_frequency)
+        if self.due_date:
+            self.due_date = self.calibration_date + relativedelta(months=+self.calibration_frequency)
         super(Equipment, self).save(*args, **kwargs)
 
     def __str__(self):
