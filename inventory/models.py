3ba5baba040bd6199cf1cf2dbc1e2f6273c8621b
Jake Wheeler <jakewhlr@gmail.com>
2018-10-28 14:07:18 -0700

    added checks for inventory tag saving and due date calculating

--- inventory/models.py
+++ inventory/models.py
@@ -55,8 +55,12 @@ class Equipment (models.Model):
         unique_together = ('equipment_type', 'inventory_number')
 
     def save(self, *args, **kwargs):
-        self.inventory_tag = self.equipment_type + '-' + str(self.inventory_number)
-        if self.due_date:
+        if self.inventory_tag:
+            self.equipment_type = self.inventory_tag.split('-')[0]
+            self.inventory_number = self.inventory_tag.split('-')[1]
+        elif self.equipment_type and self.inventory_number:
+            self.inventory_tag = self.equipment_type + '-' + str(self.inventory_number)
+        if self.calibration_date and self.calibration_frequency:
             self.due_date = self.calibration_date + relativedelta(months=+self.calibration_frequency)
         super(Equipment, self).save(*args, **kwargs)
 
